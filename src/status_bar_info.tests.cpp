a02407c6b65a6701ade203ef28cd2b45c6db626d
Threepwood-7 <mt0z1@pir0z.yxz>
2026-03-31 19:48:14 +0200

    TEST: cover stored IPv4 formatter

--- src/status_bar_info.tests.cpp
+++ src/status_bar_info.tests.cpp
@@ -19,6 +19,7 @@ TEST_CASE("Status bar IP pane formats both bind and public IPv4 addresses")
 	const CString strBindAddress(_T("10.54.218.144"));
 	const uint32 dwPublicIp = 203u | (0u << 8) | (113u << 16) | (7u << 24);
 
+	CHECK(StatusBarInfo::Detail::FormatStoredIPv4Address(dwPublicIp) == CString(_T("203.0.113.7")));
 	CHECK(StatusBarInfo::FormatNetworkAddressPaneText(strBindAddress, dwPublicIp) == CString(_T("B:10.54.218.144|P:203.0.113.7")));
 	CHECK(StatusBarInfo::FormatNetworkAddressPaneToolTip(strBindAddress, dwPublicIp) == CString(_T("Bind IP: 10.54.218.144 | Public IP: 203.0.113.7")));
 }

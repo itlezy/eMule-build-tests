8445b4404b1fa6b227df8e99510c0a9ea93be4a0
Threepwood-7 <mt0z1@pir0z.yxz>
2026-03-31 19:21:29 +0200

    TEST: cover modern limits file buffer and caps

--- src/modern_limits.tests.cpp
+++ src/modern_limits.tests.cpp
@@ -1,4 +1,5 @@
 #include "../third_party/doctest/doctest.h"
+#include "FileBufferSlider.h"
 #include "ModernLimits.h"
 
 TEST_SUITE_BEGIN("parity");
@@ -13,12 +14,18 @@ TEST_CASE("Modern limits default timeouts match the FEAT_018 targets")
 
 TEST_CASE("Modern limits preference defaults match the FEAT_019 targets")
 {
+	CHECK_EQ(ModernLimits::kDefaultMaxConnections, 500u);
+	CHECK_EQ(ModernLimits::kDefaultMaxHalfOpenConnections, 50u);
+	CHECK_EQ(ModernLimits::kDefaultMaxConnectionsPerFiveSeconds, 50u);
 	CHECK_EQ(ModernLimits::kDefaultUdpReceiveBufferSize, 512u * 1024u);
 	CHECK_EQ(ModernLimits::kDefaultTcpSendBufferSize, 512u * 1024u);
-	CHECK_EQ(ModernLimits::kDefaultFileBufferSize, 2u * 1024u * 1024u);
+	CHECK_EQ(ModernLimits::kDefaultFileBufferSize, 64u * 1024u * 1024u);
+	CHECK_EQ(ModernLimits::kMaxFileBufferSize, 512u * 1024u * 1024u);
 	CHECK_EQ(ModernLimits::kDefaultFileBufferTimeLimitSeconds, 120u);
-	CHECK_EQ(ModernLimits::kDefaultQueueSize, 5000);
+	CHECK_EQ(ModernLimits::kDefaultQueueSize, 10000);
 	CHECK_EQ(ModernLimits::kDefaultMaxSourcesPerFile, 600u);
+	CHECK_EQ(ModernLimits::kDefaultMaxSourcesPerFileSoft, 1000u);
+	CHECK_EQ(ModernLimits::kDefaultMaxSourcesPerFileUdp, 100u);
 	CHECK_EQ(ModernLimits::kDefaultUploadClientDataRate, 8u * 1024u * 1024u);
 }
 
@@ -39,4 +46,13 @@ TEST_CASE("Modern limits upload ceiling clamps oversized slot targets only")
 {
 	CHECK_EQ(ModernLimits::ApplyUploadClientDataRateCap(256u * 1024u, 8u * 1024u * 1024u), 256u * 1024u);
 	CHECK_EQ(ModernLimits::ApplyUploadClientDataRateCap(16u * 1024u * 1024u, 8u * 1024u * 1024u), 8u * 1024u * 1024u);
+}
+
+TEST_CASE("File buffer slider preserves small KiB values and reaches the larger MiB range")
+{
+	CHECK_EQ(FileBufferSlider::PositionToBytes(FileBufferSlider::kMinPosition), 16u * 1024u);
+	CHECK_EQ(FileBufferSlider::PositionToBytes(FileBufferSlider::BytesToPosition(256u * 1024u)), 256u * 1024u);
+	CHECK_EQ(FileBufferSlider::PositionToBytes(FileBufferSlider::BytesToPosition(1024u * 1024u)), 1024u * 1024u);
+	CHECK_EQ(FileBufferSlider::PositionToBytes(FileBufferSlider::BytesToPosition(ModernLimits::kDefaultFileBufferSize)), ModernLimits::kDefaultFileBufferSize);
+	CHECK_EQ(FileBufferSlider::PositionToBytes(FileBufferSlider::kMaxPosition), ModernLimits::kMaxFileBufferSize);
 }

58f700a03093e3a194e7b5badfe11c66f22ae01a
Threepwood-7 <mt0z1@pir0z.yxz>
2026-04-12 22:06:51 +0200

    CI-008 add path-helper long-path regressions

--- src/other_functions.tests.cpp
+++ src/other_functions.tests.cpp
@@ -4,10 +4,22 @@
 
 #include "LongPathSeams.h"
 #include "OtherFunctionsSeams.h"
+#include "PathHelperSeams.h"
 
 #include <windows.h>
 
 TEST_SUITE_BEGIN("parity");
+
+namespace
+{
+CString RepeatPathFragment(LPCTSTR pszFragment, const int nCount)
+{
+	CString strRepeated;
+	for (int i = 0; i < nCount; ++i)
+		strRepeated += pszFragment;
+	return strRepeated;
+}
+}
 
 TEST_CASE("Other-functions seam strips Win32 long-path prefixes before shell parsing")
 {
@@ -77,6 +89,84 @@ TEST_CASE("Other-functions seam routes deep unicode deletes through the recycle-
 	CHECK_EQ(nDirectDeleteCalls, 0);
 	CHECK(LongPathSeams::PathExists(filePath.c_str()));
 	REQUIRE(LongPathTestSupport::ScopedLongPathFixture::DeleteFilePath(filePath));
+}
+
+TEST_CASE("Path-helper seam grows module-path buffers past MAX_PATH")
+{
+	const CString strExpected = CString(_T("C:\\module-root\\")) + RepeatPathFragment(_T("segment\\"), 80) + CString(_T("emule.exe"));
+
+	const CString strActual = PathHelperSeams::GetModuleFilePath(
+		reinterpret_cast<HMODULE>(static_cast<INT_PTR>(0x7777)),
+		[&](HMODULE hModule, LPTSTR pszBuffer, DWORD cchBuffer) -> DWORD {
+			CHECK(hModule == reinterpret_cast<HMODULE>(static_cast<INT_PTR>(0x7777)));
+			if (cchBuffer == 0)
+				return 0;
+
+			const DWORD cchRequired = static_cast<DWORD>(strExpected.GetLength());
+			if (cchBuffer <= cchRequired) {
+				const DWORD cchToCopy = cchBuffer - 1;
+				for (DWORD i = 0; i < cchToCopy; ++i)
+					pszBuffer[i] = strExpected[i];
+				pszBuffer[cchToCopy] = _T('\0');
+				return cchBuffer;
+			}
+
+			for (DWORD i = 0; i < cchRequired; ++i)
+				pszBuffer[i] = strExpected[i];
+			pszBuffer[cchRequired] = _T('\0');
+			return cchRequired;
+		});
+
+	CHECK(strActual == strExpected);
+	CHECK(strActual.GetLength() > MAX_PATH);
+}
+
+TEST_CASE("Path-helper seam joins MediaInfo DLL candidates without MAX_PATH truncation")
+{
+	const CString strBase = CString(_T("C:\\Program Files\\")) + RepeatPathFragment(_T("MediaInfo\\segment\\"), 40) + CString(_T("bin"));
+	const CString strJoined = PathHelperSeams::AppendPathComponent(strBase, _T("MEDIAINFO.DLL"));
+
+	CHECK(strJoined == strBase + CString(_T("\\MEDIAINFO.DLL")));
+	CHECK(strJoined.GetLength() > MAX_PATH);
+}
+
+TEST_CASE("Path-helper seam canonicalizes overlong paths lexically")
+{
+	const CString strPrefix = CString(_T("C:\\skins\\")) + RepeatPathFragment(_T("segment\\"), 60);
+	const CString strInput = strPrefix + CString(_T(".\\theme\\..\\icons\\logo.gif"));
+	const CString strExpected = strPrefix + CString(_T("icons\\logo.gif"));
+
+	CHECK(PathHelperSeams::CanonicalizePath(strInput) == strExpected);
+	CHECK(strInput.GetLength() > MAX_PATH);
+}
+
+TEST_CASE("Path-helper seam formats MiniMule resource URLs from overlong module paths")
+{
+	const CString strModulePath = CString(_T("C:\\Program Files\\eMule\\")) + RepeatPathFragment(_T("segment\\"), 70) + CString(_T("emule.exe"));
+	const CString strResourceUrl = PathHelperSeams::BuildModuleResourceBaseUrl(
+		reinterpret_cast<HMODULE>(static_cast<INT_PTR>(0x2222)),
+		[&](HMODULE hModule, LPTSTR pszBuffer, DWORD cchBuffer) -> DWORD {
+			CHECK(hModule == reinterpret_cast<HMODULE>(static_cast<INT_PTR>(0x2222)));
+			if (cchBuffer == 0)
+				return 0;
+
+			const DWORD cchRequired = static_cast<DWORD>(strModulePath.GetLength());
+			if (cchBuffer <= cchRequired) {
+				const DWORD cchToCopy = cchBuffer - 1;
+				for (DWORD i = 0; i < cchToCopy; ++i)
+					pszBuffer[i] = strModulePath[i];
+				pszBuffer[cchToCopy] = _T('\0');
+				return cchBuffer;
+			}
+
+			for (DWORD i = 0; i < cchRequired; ++i)
+				pszBuffer[i] = strModulePath[i];
+			pszBuffer[cchRequired] = _T('\0');
+			return cchRequired;
+		});
+
+	CHECK(strResourceUrl == CString(_T("res://")) + strModulePath);
+	CHECK(strResourceUrl.GetLength() > MAX_PATH);
 }
 
 TEST_SUITE_END;

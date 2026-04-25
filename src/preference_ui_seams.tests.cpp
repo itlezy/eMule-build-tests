7ed9936efdbd4596009a53bac37e6ebb28f614fb
MTU0Z <mtu@0zz.com>
2026-04-25 12:36:36 +0200

    Test preference UI seams

---
+++ src/preference_ui_seams.tests.cpp
@@ -0,0 +1,77 @@
+#include "../third_party/doctest/doctest.h"
+
+#include "PreferenceUiSeams.h"
+
+#include <climits>
+#include <vector>
+
+TEST_SUITE_BEGIN("parity");
+
+TEST_CASE("Preference UI seam validates WebServer allowed-IP lists")
+{
+	std::vector<uint32_t> addresses;
+	CString invalidToken;
+
+	CHECK(PreferenceUiSeams::TryParseAllowedRemoteIpList(CString(_T("127.0.0.1; 192.168.1.50")), addresses, invalidToken));
+	REQUIRE(addresses.size() == 2);
+	CHECK(PreferenceUiSeams::FormatAllowedRemoteIpList(addresses) == CString(_T("127.0.0.1;192.168.1.50")));
+
+	CHECK(PreferenceUiSeams::TryParseAllowedRemoteIpList(CString(_T(" ;10.0.0.5;;")), addresses, invalidToken));
+	REQUIRE(addresses.size() == 1);
+	CHECK(PreferenceUiSeams::FormatAllowedRemoteIpList(addresses) == CString(_T("10.0.0.5")));
+
+	CHECK_FALSE(PreferenceUiSeams::TryParseAllowedRemoteIpList(CString(_T("192.168.1.999")), addresses, invalidToken));
+	CHECK(invalidToken == CString(_T("192.168.1.999")));
+
+	CHECK_FALSE(PreferenceUiSeams::TryParseAllowedRemoteIpList(CString(_T("0.0.0.0")), addresses, invalidToken));
+	CHECK(invalidToken == CString(_T("0.0.0.0")));
+}
+
+TEST_CASE("Preference UI seam normalizes exposed hidden-option modes")
+{
+	CHECK(PreferenceUiSeams::NormalizeCrashDumpMode(0) == 0);
+	CHECK(PreferenceUiSeams::NormalizeCrashDumpMode(1) == 1);
+	CHECK(PreferenceUiSeams::NormalizeCrashDumpMode(2) == 2);
+	CHECK(PreferenceUiSeams::NormalizeCrashDumpMode(9) == 0);
+
+	CHECK(PreferenceUiSeams::NormalizePreviewSmallBlocks(0) == 0);
+	CHECK(PreferenceUiSeams::NormalizePreviewSmallBlocks(1) == 1);
+	CHECK(PreferenceUiSeams::NormalizePreviewSmallBlocks(2) == 2);
+	CHECK(PreferenceUiSeams::NormalizePreviewSmallBlocks(-1) == 0);
+
+	CHECK(PreferenceUiSeams::NormalizeLogFileFormat(0) == 0);
+	CHECK(PreferenceUiSeams::NormalizeLogFileFormat(1) == 1);
+	CHECK(PreferenceUiSeams::NormalizeLogFileFormat(7) == 0);
+
+	CHECK(PreferenceUiSeams::NormalizePerfLogFileFormat(0) == 0);
+	CHECK(PreferenceUiSeams::NormalizePerfLogFileFormat(1) == 1);
+	CHECK(PreferenceUiSeams::NormalizePerfLogFileFormat(3) == 0);
+}
+
+TEST_CASE("Preference UI seam bounds diagnostic numeric options")
+{
+	CHECK(PreferenceUiSeams::IsLogFileSizeKiBAllowed(0));
+	CHECK(PreferenceUiSeams::IsLogFileSizeKiBAllowed(PreferenceUiSeams::kMaxLogFileSizeKiB));
+	CHECK_FALSE(PreferenceUiSeams::IsLogFileSizeKiBAllowed(PreferenceUiSeams::kMaxLogFileSizeKiB + 1));
+	CHECK(PreferenceUiSeams::NormalizeLogFileSizeBytes(-1) == PreferenceUiSeams::kDefaultLogFileSizeBytes);
+	CHECK(PreferenceUiSeams::NormalizeLogFileSizeBytes(static_cast<int>(PreferenceUiSeams::LogFileSizeKiBToBytes(128))) == PreferenceUiSeams::LogFileSizeKiBToBytes(128));
+	CHECK(PreferenceUiSeams::NormalizeLogFileSizeBytes(INT_MAX) == PreferenceUiSeams::kDefaultLogFileSizeBytes);
+
+	CHECK(PreferenceUiSeams::IsLogBufferKiBAllowed(PreferenceUiSeams::kMinLogBufferKiB));
+	CHECK(PreferenceUiSeams::IsLogBufferKiBAllowed(PreferenceUiSeams::kMaxLogBufferKiB));
+	CHECK_FALSE(PreferenceUiSeams::IsLogBufferKiBAllowed(PreferenceUiSeams::kMinLogBufferKiB - 1));
+	CHECK(PreferenceUiSeams::NormalizeLogBufferKiB(-1) == PreferenceUiSeams::kDefaultLogBufferKiB);
+	CHECK(PreferenceUiSeams::NormalizeLogBufferKiB(static_cast<int>(PreferenceUiSeams::kMinLogBufferKiB - 1)) == PreferenceUiSeams::kDefaultLogBufferKiB);
+	CHECK(PreferenceUiSeams::NormalizeLogBufferKiB(256) == 256);
+
+	CHECK(PreferenceUiSeams::NormalizePositiveBounded(0, 50, PreferenceUiSeams::kMaxMessageSessions) == 50);
+	CHECK(PreferenceUiSeams::NormalizePositiveBounded(100, 50, PreferenceUiSeams::kMaxMessageSessions) == 100);
+	CHECK(PreferenceUiSeams::NormalizePositiveBounded(10001, 50, PreferenceUiSeams::kMaxMessageSessions) == 50);
+
+	CHECK(PreferenceUiSeams::NormalizePerfLogIntervalMinutes(1) == 1);
+	CHECK(PreferenceUiSeams::NormalizePerfLogIntervalMinutes(PreferenceUiSeams::kMaxPerfLogIntervalMinutes) == PreferenceUiSeams::kMaxPerfLogIntervalMinutes);
+	CHECK(PreferenceUiSeams::NormalizePerfLogIntervalMinutes(0) == 5);
+	CHECK(PreferenceUiSeams::NormalizePerfLogIntervalMinutes(PreferenceUiSeams::kMaxPerfLogIntervalMinutes + 1) == 5);
+}
+
+TEST_SUITE_END;
